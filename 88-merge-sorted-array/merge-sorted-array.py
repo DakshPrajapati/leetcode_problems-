bd856cf6c00ec5ce51fb303e841a7bdfd28d3c70
Daksh Prajapati <72695602+DakshPrajapati@users.noreply.github.com>
2024-10-28 14:52:03 -0400

    Time: 0 ms (100.00%) | Memory: 16.7 MB (29.87%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = 0
+        firstlen = m
+        if firstlen > 0 and n > 0:
+            while True:
+                if i>=firstlen or not nums2:
+                    break
+                elif nums1[i] <= nums2[0]:
+                    i = i + 1
+                elif nums2[0] < nums1[i]:
+                    nums1.insert(i,nums2[0])
+                    nums2.pop(0)
+                    nums1.pop()
+                    firstlen = firstlen + 1 
+        elif m == 0 and n != 0:
+            nums1.clear()
+            for i in range(0,n):
+                nums1.append(nums2[i])
+        
+        if nums2:
+            for i in range(m+n-len(nums2), len(nums1)):
+                nums1[i] = nums2[0]
+                nums2.pop(0)
+       
+            
