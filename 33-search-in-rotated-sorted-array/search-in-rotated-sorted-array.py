e61e9c9ea870dffb592e3e49130f58fdbc7fa74d
Daxx P <72695602+DakshPrajapati@users.noreply.github.com>
2024-11-18 13:16:18 -0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (18.70%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,40 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left = 0
+        right = len(nums) - 1
+        mid = int(right/2 if right%2==0 else (right//2)+1)
+        if nums[mid] == target:
+                return mid
+        while left < right:
+            if nums[mid] == target:
+                return mid
+            elif nums[left] == target:
+                return left
+            elif nums[right] == target:
+                return right
+            elif nums[mid] > nums[left]:
+                if nums[mid] >= target and nums[left] <= target:
+                    if nums[left] == target:
+                        return left
+                    right = mid - 1
+                    mid = int((left+right)/2 if (left+right)%2==0 else (left+right)//2+1)
+                else:
+                    if nums[right] == target:
+                        return right
+                    left = mid + 1
+                    mid = int((left+right)/2 if (left+right)%2==0 else (left+right)//2+1)
+            elif nums[mid] < nums[right]:
+                if nums[mid] <= target and nums[right] >= target:
+                    if nums[right] == target:
+                        return right
+                    left = mid + 1
+                    mid = int((left+right)/2 if (left+right)%2==0 else (left+right)//2+1)
+                else:
+                    if nums[left] == target:
+                        return left
+                    right = mid - 1
+                    mid = int((left+right)/2 if (left+right)%2==0 else (left+right)//2+1)
+            else:
+                break
+        return -1
