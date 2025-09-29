250ecf05f0115426ae125f7175c744787da59cb8
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2025-09-29 19:55:02 -0400

    Time: 3 ms (87.30%) | Memory: 18.9 MB (61.62%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,10 +1,8 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         for i in range(1, len(triangle)):
-            eles = triangle[i]
-            for idx, v in enumerate(eles):
-                v1, v2 = float('inf'), float('inf')
-                if 0 <= idx < len(triangle[i-1]): v1 = triangle[i-1][idx]
-                if 0 <= idx - 1 < len(triangle[i-1]): v2 = triangle[i-1][idx-1]
+            for idx, v in enumerate(triangle[i]):
+                v1 = triangle[i-1][idx] if idx < len(triangle[i-1]) else float('inf')
+                v2 = triangle[i-1][idx-1] if idx - 1 >= 0 else float('inf')
                 triangle[i][idx] = min(v1, v2) + v
         return min(triangle[-1])
