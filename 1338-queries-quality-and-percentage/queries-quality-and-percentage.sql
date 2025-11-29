8bbdb4e2d12ff6f170549bcacc0ae20cbae44618
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2025-11-29 17:11:03 -0500

    Time: 374 ms (24.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,7 @@
+
+
+SELECT query_name,
+    round(avg((rating/position)),2) as quality, 
+    round(sum(if(rating<3,1,0))/count(query_name)*100,2) as poor_query_percentage 
+FROM Queries  
+GROUP BY query_name
