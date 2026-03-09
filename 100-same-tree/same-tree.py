3e8fbdd3bae6049f303916a88489b8653ebb277d
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2026-03-09 15:48:45 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (68.80%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,20 +6,16 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        ifSame = True
-        def trav(p, q):
-            nonlocal ifSame
-            if not p and not q:
-                return
-            if not p and q:
-                ifSame = False
-                return
-            if not q and p:
-                ifSame = False
-                return 
-            if p.val != q.val:
-                ifSame = False
-            trav(p.left, q.left)
-            trav(p.right, q.right)
-        trav(p,q)
-        return ifSame
+        def match(r1, r2):
+            if not r1 and not r2:
+                return True
+            if r1 and not r2:
+                return False
+            if r2 and not r1:
+                return False
+            if r1.val != r2.val:
+                return False
+            return match(r1.left, r2.left) and match(r1.right, r2.right)
+
+        return match(p, q)
+            
