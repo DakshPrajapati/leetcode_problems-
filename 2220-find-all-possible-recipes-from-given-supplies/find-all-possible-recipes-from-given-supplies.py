e3f441fd1ef1a2e3a18ed2aefa990c4daeec1367
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2026-03-16 18:39:48 -0400

    Time: 292 ms (10.17%) | Memory: 48.7 MB (5.22%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -5,7 +5,7 @@ class Solution:
             adjMap[recipes[idx]] = val
         
         canBeMade = set()
-        seen = []
+        seen = set()
 
         def dfs(root):
             if root in supplies or root in canBeMade:
@@ -13,12 +13,12 @@ class Solution:
             if root in seen:
                 return False       
             if root in adjMap:
+                seen.add(root)
                 for c in adjMap[root]:
-                    seen.append(root)
                     if not dfs(c):
                         seen.remove(root)
                         return False
-                    seen.remove(root)
+                seen.remove(root)
                 canBeMade.add(root)    
                 return True
             return False
