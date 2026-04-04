0af9e3e83f84c8b82ad259d9f83a8ca8c0eafd55
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2026-04-04 17:32:42 -0400

    Time: 127 ms (67.50%) | Memory: 21.2 MB (68.89%) - LeetSync

--- 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.py
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.py
@@ -1,31 +1,17 @@
 class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        def isValid(base, lenString, lenS):
-            i, j = 0, 0
-            baseLen, targetLen = lenString, lenS
+        dictionary.sort(key = lambda x: (-len(x),x))
 
-            while i < baseLen and j < targetLen:
-                if base[i] == s[j]:
+        def is_subsequence(word):
+            i = 0
+            for ch in s:
+                if i < len(word) and word[i] == ch:
                     i += 1
-                j += 1
-            
-            return True if i >= len(base) else False
-
-        def countLex(s):
-            base = 256
-            res = 0
-            for c in s:
-                res = res * base + ord(c)
-            return res
-    
-        words = [[len(x), -countLex(x), x] for x in dictionary]
-        words.sort(key=lambda x: (x[0], x[1]), reverse=True)
-        lenS = len(s)
-
-        for word in words:
-            length, lex, w = word
-            if isValid(w, length, lenS):
-                return w
-            
-        return ""
+                    if i == len(word):
+                        return True
+            return False
         
+        for w in dictionary:
+            if is_subsequence(w):
+                return w
+        return ""
