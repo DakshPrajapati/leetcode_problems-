65635f38a4de3fcdbe0668172a9d217d908ba810
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2026-04-10 00:05:28 -0400

    Time: 11 ms (6.51%) | Memory: 19.2 MB (59.57%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -3,19 +3,16 @@ class Solution:
         
 
         hashmap = defaultdict()
+        hashmap2 = defaultdict()
 
         for i in range(len(s)):
             if s[i] in hashmap.keys() and hashmap[s[i]] != t[i]:
                 return False
-            else:
-                hashmap[s[i]] = t[i]
-    
-        hashmap = defaultdict()
-
-        for i in range(len(s)):
-            if t[i] in hashmap.keys() and hashmap[t[i]] != s[i]:
+            if t[i] in hashmap2.keys() and hashmap2[t[i]] != s[i]:
                 return False
             else:
-                hashmap[t[i]] = s[i]
+                hashmap[s[i]] = t[i]
+                hashmap2[t[i]] = s[i]
     
+
         return True
