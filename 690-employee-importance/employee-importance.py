2dbceb739609cf9f14bcac13ac499a0985b1796c
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2026-04-11 18:16:19 -0400

    Time: 126 ms (66.90%) | Memory: 20.8 MB (66.11%) - LeetSync

--- 690-employee-importance/employee-importance.py
+++ 690-employee-importance/employee-importance.py
@@ -9,26 +9,6 @@ class Employee:
 
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        '''
-
-        step 1 - we need to find id of the employee in tree 
-        in case we dont find we can retuirn 0
-
-        root value start 
-        we can use deque
-
-        q.append(emp[0])
-
-        while q and we dont find id:
-            q.apend(emp[0].children) #we can have check here as well 
-            we find root:
-                savve root
-            we break 
-
-        after getting val
-        we can clear queue and start cal
-        '''
-
         mapp = { }
         importance = {}
         for i in range(len(employees)):
