a9042cdbf09f903fd1777d9ece2654b40265ff13
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2026-08-03 13:41:21 -0400

    Time: 325 ms (10.21%) | Memory: 23.9 MB (30.92%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,27 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        l = max(weights)
+        r = sum(weights)
+
+        def returnDays(wei: int) -> int:
+            days = 1
+            curWeights = 0
+            for i in range(len(weights)):
+                if curWeights + weights[i] > wei:
+                    days += 1
+                    curWeights = weights[i]
+                else:
+                    curWeights += weights[i]
+            return days
+        
+        ans = r
+        while l <= r:
+            m = (r + l) // 2
+            d = returnDays(m)
+            if d <= days:
+                r = m - 1
+                ans = min(ans, m)
+            else:
+                l = m + 1
+        
+        return (ans)
