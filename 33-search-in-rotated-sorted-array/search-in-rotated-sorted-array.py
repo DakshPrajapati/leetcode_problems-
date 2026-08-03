14ebe7a7a529fa71dbd7da6d6db37ca53eefa2e9
Daxx <72695602+DakshPrajapati@users.noreply.github.com>
2026-08-03 13:59:45 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.22%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,30 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
         left = 0
         right = len(nums) - 1
-        mid = int(right/2 if right%2==0 else (right//2)+1)
-        if nums[mid] == target:
-                return mid
-        while left < right:
-            if nums[mid] == target:
-                return mid
-            elif nums[left] == target:
-                return left
-            elif nums[right] == target:
-                return right
-            elif nums[mid] > nums[left]:
-                if nums[mid] >= target and nums[left] <= target:
-                    right = mid - 1
+
+        while left <= right:
+            middle = left + (right - left) // 2
+
+            if nums[middle] == target:
+                return middle
+
+            # Left half is sorted.
+            if nums[left] <= nums[middle]:
+                if nums[left] <= target < nums[middle]:
+                    right = middle - 1
                 else:
-                    left = mid + 1
-            elif nums[mid] < nums[right]:
-                if nums[mid] <= target and nums[right] >= target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+                    left = middle + 1
+
+            # Right half is sorted.
             else:
-                break
-            mid = int((left+right)/2 if (left+right)%2==0 else (left+right)//2+1)
-            
+                if nums[middle] < target <= nums[right]:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+
         return -1
